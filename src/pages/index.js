2fd5ea85e775dcaf7b095d14fad0068ece4fce3e
eliott.leduc <eliott.leduc@a2display.fr>
2023-05-12 14:11:37 +0200

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,175 +1,133 @@
-import * as React from "react"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import React, { useEffect, useState } from 'react';
+import { ListGroup, Form, Button, ToggleButton } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const IndexPage = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    // const requestOption = {
+    //   method: 'GET',
+    //   headers: { 'Content-Type': 'application/json' },
+    // };
+    // fetch('http://localhost:3000/api/tasks', requestOption)
+    //   .then((response) => response.json())
+    //   .then((data) => setTasks(data));
+  }, []);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    const requestOption = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: newTask, done: false }),
+    };
+    fetch('http://localhost:3000/api/tasks', requestOption)
+    if (newTask.trim()) {
+      setTasks([...tasks, { title: newTask, done: false }]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const requestOption = {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    };
+    fetch(`http://localhost:3000/api/tasks/${tasks[index]._id}`, requestOption) 
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].done = !updatedTasks[index].done;
+    setTasks(updatedTasks);
+  };
+
+  const modifyTask = (index, title, e) => {
+    e.preventDefault();
+    const requestOption = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: title, done: tasks[index].done }),
+    };
+    fetch(`http://localhost:3000/api/tasks/${tasks[index]._id}`, requestOption)
+    const updatedTasks = [...tasks];
+    updatedTasks[index].title = title;
+    updatedTasks[index].modifyTask = false;
+    console.log('updatedTasks', updatedTasks[index])
+    setTasks(updatedTasks);
+  };
+
+  const toggleModifyTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].modifyTask = !updatedTasks[index].modifyTask;
+    setTasks(updatedTasks);
+  };
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
+    <main>
+      <h1>Task List</h1>
+      <Form onSubmit={(e) => addTask(e)}>
+        <Form.Group>
+          <Form.Label>Add a new task</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter a new task"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" className='my-2 mx-2'>
+          Add Task
+        </Button>
+      </Form>
+      <ListGroup>
+        {tasks.map((task, index) => (
+          <ListGroup.Item key={index} variant={task.done && "success"}>
+            <ToggleButton 
+              type="checkbox"
+              variant={'outline-secondary'}
+              checked={task.done}
+              onClick={() => toggleTask(index)}
+            >
+              Done
+            </ToggleButton>
+            { task.modifyTask ? (
+              <>
+                <Form onSubmit={(e) => modifyTask(index, e.target[0].value, e)}>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter a new task"
+                      defaultValue={task.title}
+                    />
+                  </Form.Group>
+                  <Button variant="warning" type="submit">
+                    Cancel
+                  </Button>
+                </Form>
+              </>
+            ) : (
+              <>
+                <h2>{task.title}</h2>
+                <Button variant="warning" onClick={() => toggleModifyTask(index)}>
+                  Modify
+                </Button>
+              </>
+            )}
+            <Button variant="danger" onClick={() => deleteTask(index)} className='mx-2'>
+              Delete
+            </Button>
+          </ListGroup.Item> 
         ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      </ListGroup>
     </main>
-  )
-}
+  );
+};
 
 export default IndexPage
 
